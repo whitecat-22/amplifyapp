79301ab14937245df683f0ed1e806b1cb36d7c1b
_whitecat_22 <74803154+whitecat-22@users.noreply.github.com>
2022-04-30 21:04:51 +0900

    Add files via upload

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3074a172b": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
